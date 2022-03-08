8718835ff621238556840e3ea902688a93006097
Takahiro F <99631358+furubay@users.noreply.github.com>
2022-03-08 16:21:01 +0900

    URL変更

--- hidakaya/ContentView.swift
+++ hidakaya/ContentView.swift
@@ -6,21 +6,13 @@
 //
 
 import SwiftUI
-
+ 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            if let url = URL(string: "https://hidakaya.herokuapp.com/get") {
-                Link("日高屋ガチャへ進む", destination: url)
-                    .padding(.bottom)
-                    
-            }
-            Text("(C)2019ShoMaruyama")
-                .padding(.top)
-        }
+        WebView()
     }
 }
-
+ 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
